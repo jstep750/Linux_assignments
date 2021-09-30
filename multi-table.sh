6d20528a145c5ea77efd36857d1e077c366dd919
jstep750 <jstep0000@gmail.com>
2021-10-01 05:19:38 +0900

    make multiplication

--- multi-table.sh
+++ multi-table.sh
@@ -14,3 +14,13 @@ echo "$1 * $2 = $mul"
 echo "$1 - $2 = $minus"
 echo "$1 / $2 = $div"
 
+for ((v1=1 ; v1 <= $1 ; v1++));
+do
+  res=" "
+  for ((v2=1 ; v2 <= $2 ; v2++));
+  do
+    mult=`expr $v1 \* $v2`
+    res="$res $v1*$v2=$mult"
+  done
+    echo $res
+done
