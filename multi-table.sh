370550b479f9516c5307a315972c3bc9640ee6d3
jstep750 <jstep0000@gmail.com>
2021-10-01 05:22:42 +0900

    complete

--- multi-table.sh
+++ multi-table.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo "인 자 개 수 : $#"
-echo "1번 인 자 : $1"
-echo "2번 인 자 : $2"
-echo "모 든 인 자 내용 : $@"
-
-add=`expr $1 + $2`
-minus=`expr $1 - $2`
-mul=`expr $1 \* $2`
-div=`expr $1 / $2`
-echo "$1 + $2 = $add"
-echo "$1 * $2 = $mul"
-echo "$1 - $2 = $minus"
-echo "$1 / $2 = $div"
-
 for ((v1=1 ; v1 <= $1 ; v1++));
 do
   res=" "
