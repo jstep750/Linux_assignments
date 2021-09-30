ebbecee3dfff8c01e4223d7d3b5d6f91530b02bd
jstep750 <jstep0000@gmail.com>
2021-10-01 05:30:34 +0900

    error handling

--- multi-table.sh
+++ multi-table.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-for ((v1=1 ; v1 <= $1 ; v1++));
-do
-  res=" "
-  for ((v2=1 ; v2 <= $2 ; v2++));
-  do
-    mult=`expr $v1 \* $v2`
-    res="$res $v1*$v2=$mult"
-  done
-    echo $res
-done
+if [ $# -ne 2 ]
+then
+    echo "인자가 $#개 입력되었습니다."
+    echo "두 개의 인자를 입력해주세요!"
+else
+    echo ""
+    for ((v1=1 ; v1 <= $1 ; v1++));
+    do
+        res=" "
+        for ((v2=1 ; v2 <= $2 ; v2++));
+            do
+                mult=`expr $v1 \* $v2`
+                res="$res $v1*$v2=$mult"
+            done
+        echo $res
+    done
+    echo ""
+fi
+
+
